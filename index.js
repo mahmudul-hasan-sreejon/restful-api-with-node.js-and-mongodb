b155839364c959599f0c56bdb703834156f4c6c6
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-11-08 00:51:25 +0600

    route and query string parameters

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -9,6 +8,14 @@ app.get("/", (req, res) => {
 
 app.get("/api/courses", (req, res) => {
     res.send([9, 8, 7]);
+});
+
+app.get("/api/courses/:name/:id", (req, res) => {
+    // /api/courses/math/01
+    res.send(req.params);
+
+    // /api/courses/math/01?sortBy=name
+    res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
