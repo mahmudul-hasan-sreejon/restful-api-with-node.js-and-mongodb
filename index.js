48f53bfa45575289900b24cca15eb0552587fe4e
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-11-08 00:29:40 +0600

    nodemon testing

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world!!");
+    res.send("Hello World!!!");
 });
 
 app.get("/api/courses", (req, res) => {
-    res.send([1, 2, 3]);
+    res.send([9, 8, 7]);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
