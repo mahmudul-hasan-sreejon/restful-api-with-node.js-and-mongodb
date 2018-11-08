5efacaf47ad036cf0287795229b1f779774d1a3e
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-11-08 14:08:17 +0600

    http PUT method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -18,6 +19,7 @@ const courses = [
     }
 ];
 
+// GET Method
 app.get("/", (req, res) => {
     res.send("Hello World!!!");
 });
@@ -28,21 +30,18 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    
     if(!course) res.status(404).send("Not found...");
-    res.send(course);
+    else res.send(course);
 });
 
+// POST Method
 app.post("/api/courses", (req, res) => {
-    if(!req.body.name) {
-        res.status(400).send("Please add a name...");
+    const result = validateCourse(req.body);
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
         return;
     }
-    if(req.body.name.length < 3) {
-        res.status(100).send("Enter a big name...");
-        return;
-    }
-    
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -51,6 +50,33 @@ app.post("/api/courses", (req, res) => {
 
     res.send(course);
 });
+
+// PUT Method
+app.put("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) {
+        res.status(404).send("Not found...");
+        return;
+    }
+
+    const result = validateCourse(req.body);
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
