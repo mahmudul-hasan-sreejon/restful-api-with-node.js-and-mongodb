bf625fce6146e64b7e358e9f4e319d84be996b2a
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-11-09 13:10:35 +0600

    new validation methods added

--- index.js
+++ index.js
@@ -1,82 +1,66 @@
-const Joi = require("joi");
+const bodyParser = require("body-parser");
 const express = require("express");
+// create express app
 const app = express();
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 
-app.use(express.json());
-
-const courses = [
-    {
-        id: 1,
-        name: "course 1"
-    },
-    {
-        id: 2,
-        name: "course 2"
-    },
-    {
-        id: 3,
-        name: "course 3"
-    }
-];
+/////////////////////////////////////////// Routes ////////////////////////////////////////////////
 
 // GET Method
 app.get("/api/courses", (req, res) => {
-    res.send(courses);
+    res.send("Hello world!!!");
 });
 
-app.get("/api/courses/:id", (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("Not found...");
+// app.get("/api/courses/:id", (req, res) => {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course) return res.status(404).send("Not found...");
 
-    res.send(course);
-});
+//     res.send(course);
+// });
 
 // POST Method
-app.post("/api/courses", (req, res) => {
-    const result = validateCourse(req.body);
-    if(result.error) return res.status(400).send(result.error.details[0].message);
+// app.post("/api/courses", (req, res) => {
+//     const result = validateCourse(req.body);
+//     if(result.error) return res.status(400).send(result.error.details[0].message);
 
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
+//     const course = {
+//         id: courses.length + 1,
+//         name: req.body.name
+//     };
+//     courses.push(course);
 
-    res.send(course);
-});
+//     res.send(course);
+// });
 
 // PUT Method
-app.put("/api/courses/:id", (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("Not found...");
+// app.put("/api/courses/:id", (req, res) => {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course) return res.status(404).send("Not found...");
 
-    const result = validateCourse(req.body);
-    if(result.error) return res.status(400).send(result.error.details[0].message);
+//     const result = validateCourse(req.body);
+//     if(result.error) return res.status(400).send(result.error.details[0].message);
 
-    course.name = req.body.name;
+//     course.name = req.body.name;
 
-    res.send(course);
-});
+//     res.send(course);
+// });
 
 // DELETE Method
-app.delete("/api/courses/:id", (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("Not found...");
+// app.delete("/api/courses/:id", (req, res) => {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course) return res.status(404).send("Not found...");
 
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+//     const index = courses.indexOf(course);
+//     courses.splice(index, 1);
 
-    res.send(course);
-});
+//     res.send(course);
+// });
+/////////////////////////////////////////// Routes ////////////////////////////////////////////////
 
-// for validating body object
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(course, schema);
-}
-
+// Port mapping;
 const port = process.env.PORT || 3000;
+// listen for requests
 app.listen(port, () => console.log(`Listening on port ${port}...`));
